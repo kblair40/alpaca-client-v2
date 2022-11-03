b3ec661a68ce8441ee8c74173db06385aaa14aed
Kevin Blair <kblair40@gmail.com>
2022-11-03 17:03:40 -0500

    Add Home component and setup routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route, Link } from "react-router-dom";
+import { Box, Flex, Text, Button, useColorModeValue } from "@chakra-ui/react";
+
+import Home from "./pages/Home";
+
 function App() {
-  return <div></div>;
+  const mainBg = useColorModeValue("#fff", "gray.900");
+  return (
+    <Box minH="100vh" bg={mainBg}>
+      <Box h="100%" w="100%" position="relative">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Box>
+    </Box>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
-import theme from "utils/theme";
+import theme from "./utils/theme";
 import App from "./App";
 import "./index.css";
 
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <ChakraProvider theme={theme}>
-      {/* <ColorModeScript initialColorMode={theme.config.initialColorMode} /> */}
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </ChakraProvider>
   </BrowserRouter>
