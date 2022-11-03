04014ae03f27ef1f997dc0a1f3c78acda50a7466
Kevin Blair <kblair40@gmail.com>
2022-11-03 16:51:07 -0500

    Add api file

---
+++ src/api/index.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3001/",
+});
+
+api.interceptors.request.use(
+  // called everytime we make a request
+  async (config) => {
+    const my_token = await window.localStorage.getItem("auth-token");
+
+    if (my_token) {
+      config.headers!.Authorization = `Bearer ${my_token}`;
+    }
+    return config;
+  },
+  // called only when there is an error in the request
+  (err) => {
+    console.log("ERROR IN AXIOS INSTANCE");
+    return Promise.reject(err);
+  }
+);
+
+export default api;
