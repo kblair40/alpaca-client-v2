cb808a540492b11825408e78d010fefb09ba65b7
Kevin Blair <kblair40@gmail.com>
2022-11-06 17:21:24 -0600

    Add ExclamationMark icon if user is not authenticated with alpaca

--- src/components/Navbar/AlpacaButton.tsx
+++ src/components/Navbar/AlpacaButton.tsx
@@ -1,6 +1,7 @@
 import { Box, useTheme, useColorModeValue } from "@chakra-ui/react";
 
 import { AlpacaLogoIcon, ExclamationIcon } from "utils/icons";
+import useSelector from "hooks/useSelector";
 
 const AlpacaButton = () => {
   const {
@@ -8,15 +9,19 @@ const AlpacaButton = () => {
   } = useTheme();
   const exclamationFill = useColorModeValue(red["600"], red["500"]);
 
+  const { authenticated } = useSelector((st) => st.user);
+
   return (
     <Box position="relative">
       <Box zIndex={1}>
         <AlpacaLogoIcon boxSize="32px" />
       </Box>
 
-      <Box position="absolute" top="-6px" left="-2px" zIndex={2}>
-        <ExclamationIcon boxSize="12px" fill={exclamationFill} />
-      </Box>
+      {authenticated && !authenticated.alpaca ? (
+        <Box position="absolute" top="-6px" left="-2px" zIndex={2}>
+          <ExclamationIcon boxSize="12px" fill={exclamationFill} />
+        </Box>
+      ) : null}
     </Box>
   );
 };
