131d8a7c179f040e590a246c0d49983cb8c79b0f
Kevin Blair <kblair40@gmail.com>
2022-11-07 17:54:31 -0600

    add skeleton for DrawerSection component

--- src/components/Drawer/DrawerSection/DrawerSection.tsx
+++ src/components/Drawer/DrawerSection/DrawerSection.tsx
@@ -1,13 +1,47 @@
 import React from "react";
-import { Flex, Box, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Box,
+  Text,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+} from "@chakra-ui/react";
+
+import { MoreVerticalIcon } from "utils/icons";
 
 type Props = {
   label: string;
   children?: JSX.Element;
 };
 
-const DrawerSection = ({ children }: Props) => {
-  return <Box>/Box>
+const DrawerSection = ({ children, label }: Props) => {
+  return (
+    <Box>
+      <Flex justify={{ md: "space-between" }}>
+        <Text fontWeight={600} textTransform="uppercase">
+          {label}
+        </Text>
+
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            icon={<MoreVerticalIcon boxSize="20px" />}
+          />
+
+          <MenuList>
+            <MenuItem>Item 1</MenuItem>
+            <MenuItem>Item 2</MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+
+      <Box>{children}</Box>
+    </Box>
+  );
 };
 
 export default DrawerSection;
