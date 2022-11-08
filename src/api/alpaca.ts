5c0627ce78e66bc3aa6ac308dcea0838d7cc4542
Kevin Blair <kblair40@gmail.com>
2022-11-08 05:31:31 -0600

    Fix auth token formatting

--- src/api/alpaca.ts
+++ src/api/alpaca.ts
@@ -1,14 +1,20 @@
 import axios from "axios";
 
-const token = window.localStorage.getItem("alpaca-token");
+let token = window.localStorage.getItem("alpaca-token");
+console.log("\nTOKEN BEFORE:", token);
+
+if (token) {
+  // token = `Bearer ${token.slice(1, -1)}`;
+  token = `Bearer ${token}`;
+}
+
+console.log("\nTOKEN AFTER:", token);
 
 export const api = axios.create({
   baseURL: "https://api.alpaca.markets/v2",
   headers: {
     Authorization: token,
-    "Content-Type": "application/x-www-form-urlencoded",
   },
-  // mode: "cors",
 });
 
 export default api;
