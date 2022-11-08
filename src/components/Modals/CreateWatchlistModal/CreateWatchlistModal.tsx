a5a3e891c30dec6f34d712f22b2f9982e780b0e0
Kevin Blair <kblair40@gmail.com>
2022-11-08 12:10:48 -0600

    Finish functionality for adding tickers to watchlist

--- src/components/Modals/CreateWatchlistModal/CreateWatchlistModal.tsx
+++ src/components/Modals/CreateWatchlistModal/CreateWatchlistModal.tsx
@@ -29,7 +29,7 @@ const CreateWatchlistModal = ({ isOpen, onClose }: Props) => {
   const [name, setName] = useState("");
   const [loading, setLoading] = useState(false);
   const [allAssets, setAllAssets] = useState<any[]>();
-  const [addedSymbols, setAddedSymbols] = useState<string[]>();
+  const [addedSymbols, setAddedSymbols] = useState<string[]>([]);
 
   const bg = useColorModeValue("gray.50", "gray.900");
   const helperTextColor = useColorModeValue("gray.700", "gray.200");
@@ -39,19 +39,28 @@ const CreateWatchlistModal = ({ isOpen, onClose }: Props) => {
   useEffect(() => {
     const fetchAssets = async () => {
       try {
-        // 31,542 -> 11,108
         const response = await alpacaApi.get("/assets", {
           params: {
             status: "active",
+            // docs say us_equity is default, however as of 11/08/22,
+            //  crypto is still being returned if us_equity is not specified
+            asset_class: "us_equity",
           },
         });
         console.log("ASSETS RESPONSE.DATA:", response.data);
-        const tradableAssets = response.data.filter((asset: IAsset) => {
-          console.log("ASSET:", asset);
-          return asset.tradable;
-        });
-        console.log("COUNT:", tradableAssets.length);
-        setAllAssets(tradableAssets);
+
+        if (response.data) {
+          let tradableAssets: string[] = [];
+
+          response.data.forEach((asset: IAsset) => {
+            if (asset.tradable) {
+              tradableAssets.push(asset.symbol);
+            }
+          });
+
+          console.log("COUNT:", tradableAssets.length);
+          setAllAssets(tradableAssets);
+        }
       } catch (e) {
         console.log("FAILED TO FETCH ASSETS:", e);
       }
@@ -60,19 +69,29 @@ const CreateWatchlistModal = ({ isOpen, onClose }: Props) => {
     fetchAssets();
   }, []);
 
-  const handleAddTicker = () => {
-    //
+  const handleAddTicker = (symbol: string) => {
+    setAddedSymbols((prev: string[]) => [...prev, symbol]);
+    // handleKeyDown performs null check.  '!' is fine here.
+    symbolInputRef.current!.value = "";
   };
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
     if (!symbolInputRef.current) return;
 
-    const { value } = symbolInputRef.current;
-    if (e.key === "Enter" && value.length) {
+    if (e.key === "Enter") {
+      const { value } = symbolInputRef.current;
+
+      if (value.length && isValid(value)) {
+        handleAddTicker(value);
+      }
     }
   };
 
-  // const isValid =
+  const isValid = (symbol: string) => {
+    const isIncluded = allAssets?.includes(symbol.toUpperCase());
+    console.log(`${symbol} is included? ${isIncluded}`);
+    return isIncluded;
+  };
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -91,7 +110,7 @@ const CreateWatchlistModal = ({ isOpen, onClose }: Props) => {
             <Box>
               <FormControl isRequired>
                 <FormLabel>Tickers</FormLabel>
-                <Input ref={symbolInputRef} />
+                <Input ref={symbolInputRef} onKeyDown={handleKeyDown} />
                 <FormHelperText
                   mt="4px"
                   fontSize="13px"
@@ -101,7 +120,13 @@ const CreateWatchlistModal = ({ isOpen, onClose }: Props) => {
                 </FormHelperText>
               </FormControl>
 
-              <Stack direction="row" wrap="wrap"></Stack>
+              <Stack direction="row" wrap="wrap" mt="1rem">
+                {addedSymbols && addedSymbols.length
+                  ? addedSymbols.map((symbol, i) => {
+                      return <SymbolChip symbol={symbol} key={i} />;
+                    })
+                  : null}
+              </Stack>
             </Box>
           </Stack>
         </ModalBody>
@@ -119,3 +144,20 @@ const CreateWatchlistModal = ({ isOpen, onClose }: Props) => {
 };
 
 export default CreateWatchlistModal;
+
+const SymbolChip = ({ symbol }: { symbol: string }) => {
+  const bg = useColorModeValue("gray.200", "gray.600");
+  return (
+    <Box
+      bg={bg}
+      p="4px"
+      borderRadius="3px"
+      lineHeight={1}
+      textTransform="uppercase"
+      fontWeight="600"
+      fontSize="13px"
+    >
+      {symbol}
+    </Box>
+  );
+};
