1925604dda8c6f14ec37fc49f96c2f435ceab92c
Kevin Blair <kblair40@gmail.com>
2022-11-08 09:35:34 -0600

    Add skeletons for delete and edit modals

---
+++ src/components/Watchlists/Watchlist/WatchlistMenu/EditModal.tsx
@@ -0,0 +1,17 @@
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from "@chakra-ui/react";
+
+type Props = {};
+
+const EditModal = (props: Props) => {
+  return <div>EditModal</div>;
+};
+
+export default EditModal;
