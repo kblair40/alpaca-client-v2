28b148e84e40aeca15fc62b1ac5c4a26f5c629a8
Kevin Blair <kblair40@gmail.com>
2022-11-08 05:47:50 -0600

    Add working api request to fetch user's watchlists

--- src/components/Watchlist/Watchlist.tsx
+++ src/components/Watchlist/Watchlist.tsx
@@ -1,15 +1,30 @@
 import { useEffect, useState } from "react";
+import { Box, Text, Flex } from "@chakra-ui/react";
 
 import alpacaApi from "api/alpaca";
 
-type Props = {};
+const Watchlist = () => {
+  const [loading, setLoading] = useState(false);
+  const [watchlists, setWatchlists] = useState<any[]>();
 
-const Watchlist = (props: Props) => {
   useEffect(() => {
     const fetchWatchlists = async () => {
       try {
         const response = await alpacaApi.get("/watchlists");
         console.log("WATCHLIST RESPONSE.DATA:", response.data);
+
+        if (response.data && response.data.length) {
+          let promises = [];
+          for (let wl of response.data) {
+            promises.push(alpacaApi.get(`/watchlists/${wl.id}`));
+          }
+          let responses = await Promise.all(promises);
+          if (responses && responses.length) {
+          }
+          console.log("\n\nRESPONSES:", responses);
+        } else {
+          setWatchlists([]);
+        }
       } catch (e) {
         console.log("FAILED FETCHING WATCHLIST:", e);
       }
@@ -18,7 +33,7 @@ const Watchlist = (props: Props) => {
     fetchWatchlists();
   }, []);
 
-  return <div>Watchlist</div>;
+  return <Box></Box>;
 };
 
 export default Watchlist;
