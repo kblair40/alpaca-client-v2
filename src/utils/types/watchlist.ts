795dcc8ee5fc95924dd43f83f6ac18dea072300b
Kevin Blair <kblair40@gmail.com>
2022-11-08 06:02:41 -0600

    Add typing for IWatchlist and IWatchlistAsset

---
+++ src/utils/types/watchlist.ts
@@ -0,0 +1,20 @@
+interface IWatchlistAsset {
+  // This can be improved... https://alpaca.markets/docs/api-references/trading-api/watchlist/#watchlist-entity
+  class: string;
+  easy_to_borrow: boolean;
+  exchange: string;
+  id: string;
+  marginable: boolean;
+  shortable: boolean;
+  symbol: string;
+  tradable: boolean;
+}
+
+export interface IWatchlist {
+  id: string;
+  account_id: string;
+  created_at: string;
+  updated_at: string;
+  name: string;
+  assets: IWatchlistAsset[];
+}
