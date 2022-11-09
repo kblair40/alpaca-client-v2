114a25ae1101dc487ea5b3f46ad5f002c117fbb8
Kevin Blair <kblair40@gmail.com>
2022-11-09 13:51:20 -0600

    add skeleton for line chart component

---
+++ src/components/Chart/LineChart/LineChart.tsx
@@ -0,0 +1,18 @@
+import {
+  LineChart as ReLineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+type Props = {};
+
+const LineChart = (props: Props) => {
+  return <div>LineChart</div>;
+};
+
+export default LineChart;
