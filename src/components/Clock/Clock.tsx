35148b8311cfa08e11ef18a543758d1bb2a4dcdc
Kevin Blair <kblair40@gmail.com>
2022-11-10 11:13:55 -0600

    Finish basic clock functionality

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 import { type Clock as IClock } from "utils/types/clock";
 import { fetchCalendarData } from "store/calendarSlice";
@@ -9,6 +9,7 @@ import useSelector from "hooks/useSelector";
 
 const Clock = () => {
   const [clockData, setClockData] = useState<IClock | null>(null);
+  const [timeTo, setTimeTo] = useState("");
   const dispatch = useDispatch();
 
   const { data } = useSelector((st) => st.calendar);
@@ -23,6 +24,7 @@ const Clock = () => {
       const { next_open, next_close, is_open } = data.clock;
       const timeRes = getTimeToNextOpenAndClose(next_open, next_close, is_open);
       console.log("\n\nTIME RES:", timeRes, "\n\n");
+      setTimeTo(timeRes);
     }
   }, [data]);
 
@@ -35,7 +37,7 @@ const Clock = () => {
       zIndex={100000}
       lineHeight={1}
     >
-      Clock
+      {timeTo ? timeTo : ""}
     </Box>
   );
 };
