b527e7ead8f31e729009fd2e69df6dc62725171e
Kevin Blair <kblair40@gmail.com>
2022-11-10 11:11:16 -0600

    Move getTimeDiff functionality to separate/own function

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,4 @@
 import dayjs from "dayjs";
-// import relativeTime from "dayjs/plugin/relativeTime";
-// dayjs.extend(relativeTime);
 
 export const toTitleCase = (str: string, sep: string = " ") => {
   return str
@@ -15,6 +13,47 @@ export const toTitleCase = (str: string, sep: string = " ") => {
 const MINUTES_IN_DAY = 60 * 24;
 console.log("MINUTES_IN_DAY:", MINUTES_IN_DAY);
 
+const getTimeDiff = (start: any, end: any) => {
+  let minutesTo = end.diff(start, "minute");
+  console.log("NEXT CLOSE AFTER:", minutesTo);
+  let daysTo = Math.floor(minutesTo / MINUTES_IN_DAY);
+  // console.log("DAYS TO:", daysTo);
+  let hoursTo = Math.floor(minutesTo / 60) - 24 * daysTo;
+
+  const unitMap: { [key: string]: string } = {
+    daysTo: "days",
+    hoursTo: "hours",
+    minutesTo: "minutes",
+  };
+
+  minutesTo = minutesTo - daysTo * MINUTES_IN_DAY - hoursTo * 60;
+
+  let resObj = { daysTo, hoursTo, minutesTo };
+  let validUnits: [string, number][] = Object.entries(resObj).filter(
+    (entry) => !!entry[1]
+  );
+  // console.log("VALID UNITS:", validUnits);
+  let res = "";
+  if (validUnits.length > 0) {
+    res = `${validUnits[0][1]} ${unitMap[validUnits[0][0]]}`;
+  } else {
+    return null;
+  }
+
+  if (validUnits.length === 1) {
+    // console.log("1 RES:", res);
+    return res;
+  } else if (validUnits.length >= 2) {
+    res += `, ${validUnits[1][1]} ${unitMap[validUnits[1][0]]}`;
+    if (validUnits.length === 3) {
+      res += `, ${validUnits[2][1]} ${unitMap[validUnits[2][0]]}`;
+    }
+  }
+
+  console.log("\n\nRES:", res, "\n\n");
+  return res;
+};
+
 export const getTimeToNextOpenAndClose = (
   nextOpenDate: string,
   nextCloseDate: string,
@@ -23,57 +62,12 @@ export const getTimeToNextOpenAndClose = (
   if (!nextCloseDate || !nextOpenDate) return null;
 
   if (is_open) {
-    // get time to next close
-    let nextClose = dayjs(nextCloseDate);
-    console.log("\nNEXT CLOSE BEFORE:", nextClose);
-    // let timeTo = dayjs().to(nextClose);
-    let minutesTo = nextClose.diff(dayjs(), "minute");
-    console.log("NEXT CLOSE AFTER:", minutesTo);
-    let daysTo = Math.floor(minutesTo / MINUTES_IN_DAY);
-    // let daysTo = Math.floor(MINUTES_IN_DAY / minutesTo);
-    // console.log("DAYS TO:", daysTo);
-    let hoursTo = Math.floor(minutesTo / 60) - 24 * daysTo;
-
-    // let test = dayjs(17352033).get("day");
-    // console.log("TEST:", test);
-
-    const unitMap: { [key: string]: string } = {
-      daysTo: "days",
-      hoursTo: "hours",
-      minutesTo: "minutes",
-    };
-
-    minutesTo = minutesTo - daysTo * MINUTES_IN_DAY - hoursTo * 60;
-
-    let resObj = { daysTo, hoursTo, minutesTo };
-    let validUnits: [string, number][] = Object.entries(resObj).filter(
-      (entry) => !!entry[1]
-    );
-    // console.log("VALID UNITS:", validUnits);
-    let res = "";
-    if (validUnits.length > 0) {
-      res = `${validUnits[0][1]} ${unitMap[validUnits[0][0]]}`;
-    } else {
-      return null;
-    }
-
-    if (validUnits.length === 1) {
-      // console.log("1 RES:", res);
-      return res;
-    } else if (validUnits.length >= 2) {
-      res += `, ${validUnits[1][1]} ${unitMap[validUnits[1][0]]}`;
-      if (validUnits.length === 3) {
-        res += `, ${validUnits[2][1]} ${unitMap[validUnits[2][0]]}`;
-      }
-    }
-
-    console.log("\n\nRES:", res, "\n\n");
-
+    let res = getTimeDiff(dayjs(), dayjs(nextCloseDate)) || "";
+    // console.log("RES:", res);
     return `Market closes in ${res}`;
+  } else {
+    let res = getTimeDiff(dayjs(), dayjs(nextOpenDate)) || "";
+    // console.log("RES:", res);
+    return `Market opens in ${res}`;
   }
-
-  // if not open, return next close, else return next open
-
-  //
-  return "";
 };
