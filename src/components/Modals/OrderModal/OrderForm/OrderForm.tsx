d79680f4d7985fa4df1ddf3ad5922cc9b44be610
Kevin Blair <kblair40@gmail.com>
2022-11-15 11:56:22 -0600

    Add helper to render each input

--- src/components/Modals/OrderModal/OrderForm/OrderForm.tsx
+++ src/components/Modals/OrderModal/OrderForm/OrderForm.tsx
@@ -10,34 +10,70 @@ import {
   FormHelperText,
   FormErrorMessage,
   Box,
+  Select,
 } from "@chakra-ui/react";
+import {
+  DEFAULT_MARKET,
+  DEFAULT_LIMIT,
+  DEFAULT_STOP,
+  DEFAULT_STOP_LIMIT,
+  TIME_IN_FORCE,
+  ORDER_TYPES,
+  LABEL_MAP,
+} from "./options";
 
-type Props = {};
+const DEFAULT_VALUES = DEFAULT_MARKET;
 
-const DEFAULT_VALUES = {
-  orderType: "market",
-  // quantity: "0",
-  quantity: 0,
-  timeInForce: "day",
-};
-
-const OrderForm = (props: Props) => {
+const OrderForm = () => {
   const [formData, setFormData] = useState(DEFAULT_VALUES);
+
+  const getLabel = (label: string) => {
+    return {
+      timeInForce: "Time in Force",
+      orderType: "Order Type",
+    };
+  };
+
+  const makeInput = (label: string) => {
+    return (
+      <FormControl isRequired>
+        <FormLabel>{LABEL_MAP[label]}</FormLabel>
+        {label === "orderType" || label === "timeInForce" ? (
+          <Select>
+            {label === "orderType"
+              ? ORDER_TYPES.map((type, i) => {
+                  return <option value={type.value}>{type.label}</option>;
+                })
+              : TIME_IN_FORCE.map((type, i) => {
+                  return <option value={type.value}>{type.label}</option>;
+                })}
+          </Select>
+        ) : (
+          <Input />
+        )}
+      </FormControl>
+    );
+    // if (label === "orderType" || label === "timeInForce") {
+    //   return (
+    //     <FormControl isRequired>
+    //       <FormLabel>{LABEL_MAP[label]}</FormLabel>
+    //     </FormControl>
+    //   );
+    // }
+  };
 
   return (
     <Stack>
-      <FormControl isRequired>
+      {formData &&
+        Object.entries(formData).map((entry, i) => {
+          console.log("ENTRY:", entry);
+          return makeInput(entry[0]);
+          // return <Box key={i} />;
+        })}
+      {/* <FormControl isRequired>
         <FormLabel>Watchlist Name</FormLabel>
-        <Input
-          variant="neutral-outline"
-          // value={name}
-          // isInvalid={nameInvalid}
-          // onChange={(e) => {
-          //   setName(e.target.value);
-          //   if (createError) setCreateError("");
-          // }}
-        />
-      </FormControl>
+        <Select></Select>
+      </FormControl> */}
     </Stack>
   );
 };
