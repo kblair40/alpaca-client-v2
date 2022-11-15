3db5d2d9e61ef335d4cc71e7f169c5d7a0217529
Kevin Blair <kblair40@gmail.com>
2022-11-15 07:57:58 -0600

    Add open/closed state to slice

--- src/store/orderSlice.ts
+++ src/store/orderSlice.ts
@@ -3,14 +3,22 @@ import { type IWatchlistAsset } from "utils/types/watchlist";
 
 type SliceState = {
   asset: IWatchlistAsset | null; // standard asset object from alpaca;
+  showModal: boolean;
 };
 
 const orderSlice = createSlice({
   name: "order",
   initialState: {
     asset: null,
+    showModal: false,
   } as SliceState,
   reducers: {
+    openModal(state) {
+      state.showModal = true;
+    },
+    closeModal(state) {
+      state.showModal = false;
+    },
     setAsset(state, action) {
       state.asset = action.payload;
     },
