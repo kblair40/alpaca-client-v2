7bd750bf993e5de6f58d55416ee5ee678485f49e
Kevin Blair <kblair40@gmail.com>
2022-11-16 06:03:13 -0600

    Add api fetch call to get latest orders from user

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+import useDispatch from "hooks/useDispatch";
+import { fetchOrders } from "store/orderSlice";
 
 type Props = {};
 
 const Dashboard = (props: Props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchOrders());
+  }, [dispatch]);
+
   return <div>Dashboard</div>;
 };
 
--- src/store/orderSlice.ts
+++ src/store/orderSlice.ts
@@ -1,11 +1,36 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { type IWatchlistAsset } from "utils/types/watchlist";
+
+import { alpaca } from "api";
+
+export const fetchOrders = createAsyncThunk("order/fetchOrders", async () => {
+  const isAuthenticated = !!window.localStorage.getItem("auth-token");
+
+  if (isAuthenticated) {
+    try {
+      const response = await alpaca.get("/order");
+      console.log("USER ORDERS:", response.data);
+
+      if (response && response.data) {
+        return response.data;
+      }
+    } catch (err) {
+      console.log("FAILED:", err);
+      return [];
+    }
+  }
+
+  return [];
+});
 
 type SliceState = {
   asset: IWatchlistAsset | null; // standard asset object from alpaca;
   showModal: boolean;
   tickerSymbol: string;
   priceData: any;
+  orders: any[];
+  status: null | "loading" | "completed" | "failed";
+  error: boolean;
 };
 
 const orderSlice = createSlice({
@@ -15,6 +40,9 @@ const orderSlice = createSlice({
     showModal: false,
     tickerSymbol: "",
     priceData: null,
+    status: null,
+    error: false,
+    orders: [],
   } as SliceState,
   reducers: {
     openModal(state, action) {
@@ -33,6 +61,29 @@ const orderSlice = createSlice({
       state.priceData = null;
       state.showModal = false;
     },
+  },
+
+  extraReducers(builder) {
+    builder
+      .addCase(fetchOrders.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchOrders.fulfilled, (state, action) => {
+        state.status = "completed";
+        const data = action.payload;
+        console.log("\n\nUSER ORDERS DATA DATA:", data);
+
+        if (data) {
+          state.orders = data;
+        } else {
+          state.error = true;
+          state.status = "failed";
+        }
+      })
+      .addCase(fetchOrders.rejected, (state) => {
+        state.status = "failed";
+        state.error = true;
+      });
   },
 });
 
