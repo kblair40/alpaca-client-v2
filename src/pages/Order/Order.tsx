be21e0aa19ddbf094b57cc454c211617c7dedabd
Kevin Blair <kblair40@gmail.com>
2022-11-16 08:00:19 -0600

    Add orderData rendering to Order page

--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -2,10 +2,11 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Box, Flex, Text, Center, Spinner } from "@chakra-ui/react";
 
+import { type IOrder } from "utils/types/order";
 import { alpaca } from "api";
 
 const Order = () => {
-  const [orderData, setOrderData] = useState<any>(null);
+  const [orderData, setOrderData] = useState<IOrder | null>(null);
   const [loading, setLoading] = useState(true);
 
   const { orderId } = useParams();
@@ -15,6 +16,7 @@ const Order = () => {
       try {
         const response = await alpaca.get(`/order/${orderId}`);
         console.log("\n\nORDER RESPONSE:", response.data, "\n\n");
+        setOrderData(response.data);
       } catch (e) {
         console.log("FAILED TO FETCH ORDER:", e);
         setOrderData(null);
@@ -33,7 +35,37 @@ const Order = () => {
     );
   }
 
-  return <Box>Order</Box>;
+  if (orderData) {
+    return (
+      <Flex
+        direction="column"
+        align="center"
+        mt="2rem"
+        px={{ base: "1rem", sm: "3rem" }}
+      >
+        <Flex align="end" lineHeight={1}>
+          <Text mr="1rem" fontSize="xl" fontWeight={600}>
+            {orderData.symbol}
+          </Text>
+          <Text textTransform="capitalize">{`${orderData.type} ${orderData.side}`}</Text>
+        </Flex>
+
+        <Box w="100%">
+          <Flex mt="1.5rem">
+            <Text>Qty Ordered</Text>
+            <Text>{orderData.qty}</Text>
+          </Flex>
+
+          <Flex>
+            <Text>Qty Filled</Text>
+            <Text>{orderData.filled_qty}</Text>
+          </Flex>
+        </Box>
+      </Flex>
+    );
+  }
+
+  return null;
 };
 
 export default Order;
