0dc86276c66487bd9fcc0e3d1a0a90ad8d054bf7
Kevin Blair <kblair40@gmail.com>
2022-11-18 08:20:16 -0600

    Add constants file and initialize with light/dark mode chart colors

---
+++ src/utils/constants.ts
@@ -0,0 +1,47 @@
+export const darkModeChartColors = [
+  // .300
+  "#FC8181", // red
+  "#F6AD55", // orange
+  "#F6E05E", // yellow
+  "#68D391", // green
+  "#4FD1C5", // teal
+  "#63B3ED", // blue
+  "#76E4F7", // cyan
+  "#B794F4", // purple
+  "#F687B3", // pink
+  // .100
+  "#FED7D7", // red
+  "#FEEBC8", // orange
+  "#FEFCBF", // yellow
+  "#C6F6D5", // green
+  "#B2F5EA", // teal
+  "#BEE3F8", // blue
+  "#C4F1F9", // cyan
+  "#E9D8FD", // purple
+  "#FED7E2", // pink
+];
+
+export const lightModeChartColors = [
+  // .700
+  "#9B2C2C", // red
+  "#9C4221", // orange
+  "#975A16", // yellow
+  "#276749", // green
+  "#285E61", // teal
+  "#2C5282", // blue
+  "#0987A0", // cyan
+  "#553C9A", // purple
+  "#97266D", // pink
+  // .400
+  "#F56565", // red
+  "#ED8936", // orange
+  "#ECC94B", // yellow
+  "#48BB78", // green
+  "#38B2AC", // teal
+  "#4299E1", // blue
+  "#0BC5EA", // cyan
+  "#9F7AEA", // purple
+  "#ED64A6", // pink
+];
+
+// export const lightModeChartColors =
