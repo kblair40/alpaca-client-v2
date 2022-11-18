1d75675973c966be1ffa9d70d0a8c95ad7ddf890
Kevin Blair <kblair40@gmail.com>
2022-11-18 06:20:00 -0600

    Finish account fetch logic

--- src/components/PositionsChart/PositionsChart.tsx
+++ src/components/PositionsChart/PositionsChart.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { Box, Spinner, Center, Flex, Text } from "@chakra-ui/react";
+
+import useDispatch from "hooks/useDispatch";
+import useSelector from "hooks/useSelector";
+import { fetchAccount } from "store/accountSlice";
 
 type Props = {};
 
-const PositionsChart = (props: Props) => {
+const PositionsChart = () => {
+  const dispatch = useDispatch();
+
+  const { data, status } = useSelector((st) => st.account);
+
+  useEffect(() => {
+    dispatch(fetchAccount());
+  }, [dispatch]);
+
+  if (status === "loading") {
+    return (
+      <Center h="120px">
+        <Spinner />
+      </Center>
+    );
+  }
+
+  if (!data) return <Text>NO DATA</Text>;
+
   return <div>PositionsChart</div>;
 };
 
