e1a8778bd73becc957bf0349ec0a587a7153a78b
Kevin Blair <kblair40@gmail.com>
2022-11-21 16:35:10 -0600

    finish popover open/close logic

--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -1,5 +1,24 @@
-import React, { useState } from "react";
-import { Input, InputGroup, InputLeftElement, Tooltip } from "@chakra-ui/react";
+import React, { useState, useRef } from "react";
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Tooltip,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  PopoverHeader,
+  PopoverBody,
+  PopoverFooter,
+  PopoverArrow,
+  // PopoverCloseButton,
+  PopoverAnchor,
+  Portal,
+  Box,
+  Text,
+  Center,
+  Spinner,
+} from "@chakra-ui/react";
 
 import api from "api";
 import { SearchIcon } from "utils/icons";
@@ -12,52 +31,107 @@ interface SearchProps {
 const SearchInput = ({ isDark, isDisabled }: SearchProps) => {
   const [value, setValue] = useState("");
   const [results, setResults] = useState<any[] | null>(null);
+  const [searching, setSearching] = useState(false);
+  const [showResults, setShowResults] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
   const placeholderColor = isDark ? "gray.300" : "gray.500";
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = e;
+    const { value } = e.target;
     setValue(value);
 
-    // if (value.length) {
-    //   try {
-    //     const response = await api.get(`/search/asset/${value}`);
-    //     console.log("\n\nSEARCH RESULTS:", response.data, "\n\n");
-    //     if (response && response.data) {
-    //       setResults(response.data);
-    //     }
-    //   } catch (e) {
-    //     console.log("FAILED TO FETCH RESULTS:", e);
-    //   }
-    // }
+    if (value.length) {
+      setSearching(true);
+      try {
+        const response = await api.get(`/search/asset/${value}`);
+        console.log("\n\nSEARCH RESULTS:", response.data, "\n\n");
+        if (response && response.data) {
+          setResults(response.data);
+        } else {
+          setResults(null);
+        }
+      } catch (e) {
+        console.log("FAILED TO FETCH RESULTS:", e);
+        setResults(null);
+      }
+    } else {
+      setShowResults(false);
+      setResults(null);
+    }
+    setSearching(false);
   };
+
+  const popoverBg = isDark ? "gray.800" : "gray.50";
 
   return (
     <Tooltip label="You must be signed in" isDisabled={!isDisabled}>
-      <InputGroup maxW="260px">
-        <InputLeftElement
-          h="36px"
-          children={<SearchIcon boxSize="16px" />}
-          pointerEvents="none"
-        />
-        <Input
-          value={value}
-          onChange={handleChange}
-          isDisabled={isDisabled}
-          h="36px"
-          borderRadius="6px"
-          _placeholder={{
-            color: placeholderColor,
-            fontSize: "sm",
-            position: "relative",
-            bottom: "1px",
-          }}
-          placeholder="Search for stocks or indexes"
-        />
-      </InputGroup>
+      <Popover
+        initialFocusRef={inputRef}
+        isOpen={isFocused && value.length > 0}
+        closeOnBlur={false}
+      >
+        <PopoverTrigger>
+          <InputGroup maxW="260px">
+            <InputLeftElement
+              h="36px"
+              children={<SearchIcon boxSize="16px" />}
+              pointerEvents="none"
+            />
+
+            <Input
+              ref={inputRef}
+              value={value}
+              onChange={handleChange}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              isDisabled={isDisabled}
+              h="36px"
+              borderRadius="6px"
+              _placeholder={{
+                color: placeholderColor,
+                fontSize: "sm",
+                position: "relative",
+                bottom: "1px",
+              }}
+              placeholder="Search for stocks or indexes"
+            />
+          </InputGroup>
+        </PopoverTrigger>
+
+        <Portal>
+          <PopoverContent bg={popoverBg}>
+            <PopoverHeader>Results</PopoverHeader>
+            <PopoverBody>
+              {results && results.length ? (
+                results.map((res, i) => {
+                  return <Box key={i}>{res.symbol}</Box>;
+                })
+              ) : results && !results.length ? (
+                <Text textAlign="center">No Results</Text>
+              ) : searching ? (
+                <Center>
+                  <Spinner />
+                </Center>
+              ) : null}
+            </PopoverBody>
+            {/* <Results results={results} /> */}
+          </PopoverContent>
+        </Portal>
+      </Popover>
     </Tooltip>
   );
 };
 
 export default SearchInput;
+
+const Results = ({ results }: { results: null | any[] }) => {
+  return (
+    <PopoverBody>
+      Body Body Body Body Body Body Body Body Body Body Body Body Body Body
+    </PopoverBody>
+  );
+};
